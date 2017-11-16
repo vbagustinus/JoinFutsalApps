13b88460b4081331a011e9e169191a8c89f268e0
vbagustinus <vbagustinus@gmail.com>
2017-11-16 16:57:37 +0700

    npm mongodb

--- server/app.js
+++ server/app.js
@@ -8,6 +8,7 @@ app.use(cors())
 app.use(logger('dev'));
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+
 //
 // app.all('/*', function (req, res, next) {
 //     res.header("Access-Control-Allow-Origin", "http://127.0.0.1:8080");
