85fde0110d408cd7e3662e24bf7cb1a1fefb951c
vbagustinus <vbagustinus@gmail.com>
2017-11-17 16:57:57 +0700

    Styling

--- server/middlewares/verifyLogin.js
+++ server/middlewares/verifyLogin.js
@@ -1,7 +1,7 @@
 var jwt = require('jsonwebtoken');
 
 let isLogin = (req, res, next) =>{
-  let token = req.body.headers.jwttoken
+  let token = req.body.headers
   console.log('==========================', token);
   // verify a token symmetric
   // jwt.verify(token, 'estehpurun', function(err, decoded) {
